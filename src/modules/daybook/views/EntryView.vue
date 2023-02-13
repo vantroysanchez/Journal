4c436988621bccd9fbfd0c90baa0be49a10855ae
Vantroy Sanchez Paulino <vantroy.sanchez@intec.edu.do>
2023-02-13 16:18:50 -0400

    - commit inicial.

---
+++ src/modules/daybook/views/EntryView.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="entry-title d-flex justify-content-between p-2">
+    <div>
+      <span class="text-success fs-3 fw-bold">09</span>
+      <span class="mx-1 fs-3">Febrero</span>
+      <span class="mx-2 fs-4 fw-light">2023, jueves</span>
+    </div>
+
+    <div>
+      <button class="btn btn-danger mx-2">
+        Borrar
+        <i class="fa fa-trash-alt"></i>
+      </button>
+      <button class="btn btn-primary">
+        Subir foto
+        <i class="fa fa-upload"></i>
+      </button>
+    </div>
+  </div>
+  <hr />
+
+  <div class="d-flex flex-column px-3 h-75">
+    <textarea placeholder="¿Qué sucedió hoy?"></textarea>
+  </div>
+
+  <Fab icon="fa-save" />
+
+  <img
+    src="https://educacion30.b-cdn.net/wp-content/uploads/2019/02/girasoles-978x652.jpg"
+    alt="entry-picture"
+    class="img-thumbnail"
+  />
+</template>
+
+<script>
+import { defineAsyncComponent } from "vue";
+
+export default {
+  components: {
+    Fab: defineAsyncComponent(() => import("../components/Fab.vue")),
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+textarea {
+  border: none;
+  font-size: 20px;
+  height: 100%;
+
+  &:focus {
+    outline: none;
+  }
+}
+
+img {
+  width: 200px;
+  position: fixed;
+  bottom: 150px;
+  right: 20px;
+  box-shadow: 0px 5px 10px rgba($color: #000000, $alpha: 0.2);
+}
+</style>
