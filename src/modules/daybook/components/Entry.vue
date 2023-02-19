7640652c6f10bf19a30b03fdab014bae0e443001
vantroysanchez <vantroysanchez@gmail.com>
2023-02-19 10:55:56 -0400

    - Utilizando getters.

--- src/modules/daybook/components/Entry.vue
+++ src/modules/daybook/components/Entry.vue
@@ -1,26 +1,72 @@
 <template>
   <div
     class="entry-container mb-3 pointer p-2"
-    @click="$router.push({ name: 'entry', params: { id: 10 } })"
+    @click="$router.push({ name: 'entry', params: { id: entry.id } })"
   >
     <div class="entry-title d-flex">
-      <span class="text-success fs-5 fw-bold">19</span>
-      <span class="mx-1 fs-5">Enero</span>
-      <span class="mx-2 fw-light">2023, jueves</span>
+      <span class="text-success fs-5 fw-bold">{{ day }}</span>
+      <span class="mx-1 fs-5">{{ month }}</span>
+      <span class="mx-2 fw-light">{{ yearDay }}</span>
     </div>
     <div class="entry-description">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Non sapiente sit
-      asperiores ea rerum optio expedita repellendus error quod itaque, ullam
-      pariatur a, nostrum molestias, maiores inventore cumque. Temporibus,
-      similique!
+      {{ shortText }}
     </div>
   </div>
 </template>
 
 <script>
+const months = [
+  "Enero",
+  "Febrero",
+  "Marzo",
+  "Abril",
+  "Mayo",
+  "Junio",
+  "Julio",
+  "Agosto",
+  "Septiembre",
+  "Octubre",
+  "Noviembre",
+  "Diciembre",
+];
+const days = [
+  "Domingo",
+  "Lunes",
+  "Martes",
+  "Miércoles",
+  "Jueves",
+  "Viernes",
+  "Sábado",
+];
+
 export default {
   /* eslint-disable */
   name: "Entry",
+  props: {
+    entry: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    shortText() {
+      return this.entry.text.length > 130
+        ? this.entry.text.substring(0, 130) + "..."
+        : this.entry.text;
+    },
+    day() {
+      const date = new Date(this.entry.date);
+      return date.getDate();
+    },
+    month() {
+      const date = new Date(this.entry.date);
+      return months[date.getMonth()];
+    },
+    yearDay() {
+      const date = new Date(this.entry.date);
+      return `${date.getFullYear()}, ${days[date.getDay()]}`;
+    },
+  },
 };
 </script>
 
--- src/modules/daybook/store/journal/getters.js
+++ src/modules/daybook/store/journal/getters.js
@@ -4,8 +4,11 @@
 // }
 
 
-export const getEntriesByTerm = ( /*state*/ ) => {
- 
+export const getEntriesByTerm = ( state ) => (term = '') => {
+    
+    if(term.length === 0) return state.entries
+  
+    return state.entries.filter( entry => entry.text.toLowerCase().includes(term.toLocaleLowerCase()))
 }
 
 export const getEntryById = ( /*state*/ ) => {
