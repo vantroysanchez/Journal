487108174ac75119b618eef83ef2d08c06e3096b
vantroysanchez <vantroysanchez@gmail.com>
2023-02-28 15:39:53 -0400

    - Trabajos para completar la seccion.

--- src/modules/daybook/store/journal/state.js
+++ src/modules/daybook/store/journal/state.js
@@ -3,19 +3,19 @@ export default () => ({
     isLoading:true,
     entries: [
         {
-            id: new Date().getTime(),
+            id: '1',
             date: new Date().toDateString(),
             text: 'Laborum consequat magna sint voluptate incididunt mollit nostrud id occaecat non exercitation labore excepteur.',
             picture: null
         },
         {
-            id: new Date().getTime() + 1000,
+            id: '2',
             date: new Date().toDateString(),
             text: 'Mollit est aute veniam magna irure dolore ad incididunt culpa velit veniam qui consequat. Ut quis quis duis voluptate quis dolore amet esse ut sunt ea.',
             picture: null
         },
         {
-            id: new Date().getTime() + 2000,
+            id: '3',
             date: new Date().toDateString(),
             text: 'Velit excepteur ea irure sint amet ipsum deserunt. Consequat dolore qui cupidatat ullamco qui. Culpa laboris quis aliquip adipisicing ad.',
             picture: null
--- src/modules/daybook/views/EntryView.vue
+++ src/modules/daybook/views/EntryView.vue
@@ -1,43 +1,93 @@
 <template>
-  <div class="entry-title d-flex justify-content-between p-2">
-    <div>
-      <span class="text-success fs-3 fw-bold">09</span>
-      <span class="mx-1 fs-3">Febrero</span>
-      <span class="mx-2 fs-4 fw-light">2023, jueves</span>
+  <template v-if="entry">
+    <div class="entry-title d-flex justify-content-between p-2">
+      <div>
+        <span class="text-success fs-3 fw-bold">{{ day }}</span>
+        <span class="mx-1 fs-3">{{ month }}</span>
+        <span class="mx-2 fs-4 fw-light">{{ yearDay }}</span>
+      </div>
+
+      <div>
+        <button class="btn btn-danger mx-2">
+          Borrar
+          <i class="fa fa-trash-alt"></i>
+        </button>
+        <button class="btn btn-primary">
+          Subir foto
+          <i class="fa fa-upload"></i>
+        </button>
+      </div>
+    </div>
+    <hr />
+
+    <div class="d-flex flex-column px-3 h-75">
+      <textarea placeholder="¿Qué sucedió hoy?" v-model="entry.text">
+      </textarea>
     </div>
 
-    <div>
-      <button class="btn btn-danger mx-2">
-        Borrar
-        <i class="fa fa-trash-alt"></i>
-      </button>
-      <button class="btn btn-primary">
-        Subir foto
-        <i class="fa fa-upload"></i>
-      </button>
-    </div>
-  </div>
-  <hr />
-
-  <div class="d-flex flex-column px-3 h-75">
-    <textarea placeholder="¿Qué sucedió hoy?"></textarea>
-  </div>
+    <img
+      src="https://educacion30.b-cdn.net/wp-content/uploads/2019/02/girasoles-978x652.jpg"
+      alt="entry-picture"
+      class="img-thumbnail"
+    />
+  </template>
 
   <Fab icon="fa-save" />
-
-  <img
-    src="https://educacion30.b-cdn.net/wp-content/uploads/2019/02/girasoles-978x652.jpg"
-    alt="entry-picture"
-    class="img-thumbnail"
-  />
 </template>
 
 <script>
 import { defineAsyncComponent } from "vue";
+import { mapGetters } from "vuex";
+import getDayMonthYear from "../helpers/getDayMonthYear";
 
 export default {
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+  },
   components: {
     Fab: defineAsyncComponent(() => import("../components/Fab.vue")),
+  },
+
+  computed: {
+    ...mapGetters("journal", ["getEntryById"]),
+    day() {
+      const { day } = getDayMonthYear(this.entry.date);
+      return day;
+    },
+    month() {
+      const { month } = getDayMonthYear(this.entry.date);
+      return month;
+    },
+    yearDay() {
+      const { yearDay } = getDayMonthYear(this.entry.date);
+      return yearDay;
+    },
+  },
+
+  data() {
+    return {
+      entry: null,
+    };
+  },
+  methods: {
+    loadEntry() {
+      const entry = this.getEntryById(this.id);
+
+      if (!entry) return this.$router.push({ name: "no-entry" });
+
+      this.entry = entry;
+    },
+  },
+  created() {
+    this.loadEntry();
+  },
+  watch: {
+    id() {
+      this.loadEntry();
+    },
   },
 };
 </script>
