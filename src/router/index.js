7b30d3acd92be9250ee7accf13f7d2308f8c12ec
vantroysanchez <vantroysanchez@gmail.com>
2023-05-06 14:02:16 -0400

    - Inicio de login y authentication.

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import daybookRouter from "../modules/daybook/router";
+import authRouter from "../modules/auth/router";
 
 const routes = [
   {
@@ -15,6 +16,10 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path:'/auth',
+    ...authRouter
   },
   {
     path:'/daybook',
